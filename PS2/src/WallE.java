ef9e00d0edba61144d55f2b8ad5e800543674883
mgb5db <mgb5db@virginia.edu>
2015-03-04 23:42:17 -0500

    WallE printing glitches fixed

--- PS2/src/WallE.java
+++ PS2/src/WallE.java
@@ -49,6 +49,10 @@ public class WallE extends Robot {
 		//it will return what it has so far
 		MapPoint start = new MapPoint(0);
 		start.setLocation(this.getPosition());
+		System.out.println("START POSITION: "+start);
+		MapPoint end = new MapPoint(0);
+		end.setLocation(endPosition);
+		System.out.println("END POSITION: "+end);
 		
 		openSet.clear();
 		cameFrom.clear();
@@ -60,7 +64,7 @@ public class WallE extends Robot {
 
 		while(!openSet.isEmpty()) {
 			MapPoint next = openSet.poll();
-			System.out.println(next);
+			//System.out.println(next);
 			if(next.equals(endPosition)) {
 				// Got to target
 				// Backtrack, create path
@@ -78,19 +82,18 @@ public class WallE extends Robot {
 				for(int y = -1; y <= 1; y++){
 					MapPoint neighborPoint = new MapPoint(0);
 					neighborPoint.setLocation(x + next.x, y + next.y);
-					System.out.println(neighborPoint);
+					//System.out.println(neighborPoint);
 					if(neighborPoint.x < 0 || neighborPoint.y < 0 || neighborPoint.x > rows - 1 
 							|| neighborPoint.y > cols - 1) {
-						System.out.println("Out of bounds");
+						//System.out.println("Out of bounds");
 						continue;
 					}
 					if(closedSet.contains(neighborPoint)) {
-						System.out.println("Already tried");
-						
+						//System.out.println("Already tried");
 						continue;
 					}
 					int tenative_score = (int) (next.getBestDist() + next.distanceSq(neighborPoint));
-					System.out.println(tenative_score);
+					//System.out.println(tenative_score);
 					if(!openSet.contains(neighborPoint)) {
 						//System.out.println("best option yet");
 						cameFrom.put(neighborPoint, next);
@@ -123,34 +126,43 @@ public class WallE extends Robot {
 		// If robot gets to the end of the path and it is not the target, call plan again
 		// Else robot got to the end of the path and it is the target
 
-		System.out.println("Move the bot");
+		//System.out.println("Move the bot");
 		
 		Point current = this.getPosition();
 		Point last = current;
 		MapPoint currentMP = new MapPoint(0);
-		currentMP.setLocation(currentMP);
+		currentMP.setLocation(current);
 		System.out.println(currentMP);
 		
 		for(int x=0; x<path.size(); x++){
 			MapPoint mp = path.get(x);
+			if(mp.equals(endPosition))
+				System.out.println(mp);
 			current = this.move(mp);
 			if(current.equals(endPosition)){
 				//made it to target
-				System.out.println(mp);
-				complete = 1;
-				return;
+				//something in the jar is overriding my end case - since I am checking the result
+				//of the move command but it takes hold when move is called on the target
+				//System.out.println("got to target: "+mp);
+				//complete = 1;
+				//return;
 			}
-			else if(current == last){
+			else if(current.equals(last)){
 				//robot did not move (ran into wall)
-				break;
+				System.out.println("Ran into wall.");
+				return;
 			}
 			else if(x+1==path.size() && !current.equals(endPosition)){
 				//path did not make it to the end
 				System.out.println(mp);
-				break;
+				System.out.println("End of current path.");
+				return;
 			}
 			else{
 				//continue on path
+				//System.out.println("continue on path");
+				last = current;
+				//System.out.println(current);
 				System.out.println(mp);
 				continue;
 			}
@@ -164,11 +176,11 @@ public class WallE extends Robot {
 
 	public static void main(String[] args) {
 		try {
-			World myWorld = new World("worldFiles/optimalTest.txt", true);
+			World myWorld = new World("worldFiles/simpleWorld.txt", true);
 			WallE walle = new WallE();
 			walle.addToWorld(myWorld);
 			for(int c=0; c<5; c++){
-				System.out.println("TRYING TO FIND PATH: " + c);
+				System.out.println("TRYING TO FIND PATH: " + (c+1));
 				walle.planPath();
 				walle.travelToDestination();
 				if(complete == 1)
