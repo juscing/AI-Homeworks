ea8d9ade2da3b6662d0352a13f0b61400f3dc293
juscing <juscing@gmail.com>
2015-04-28 16:40:55 -0400

    switch it back to bayes

--- PS4/score.sh
+++ PS4/score.sh
@@ -15,7 +15,7 @@ if [ -z "$3" ]
         exit
 fi
 n=`wc -l $3 | awk '{print $1;}'`
-java -cp ./bin CoinFlip $1 $2 > TEMPCOMPARE.txt
+java -cp ./bin BayesClassifier $1 $2 > TEMPCOMPARE.txt
 diff=`sdiff -B -b -s $3 TEMPCOMPARE.txt | wc -l`
 rm TEMPCOMPARE.txt
 bc <<< "scale = 10; 100 * (1 - ($diff / $n))"
