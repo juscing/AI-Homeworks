2f013dd311e8b2532d321b30e0e7fbfb960d13aa
juscing <juscing@gmail.com>
2015-04-27 23:50:12 -0400

    Found it

--- PS4/compare.sh
+++ PS4/compare.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-awk '{print $NF}' trainingData/census.train
+awk '{print $NF}' trainingData/census.train | tee TEMPCOMPARE.txt
+n=`wc -l TEMPCOMPARE.txt | awk '{print $1;}'`
+#diff=java BayesClassifier | diff -U 0 TEMPCOMPARE.txt - | grep -v ^@ | wc -l
+#x=$diff/$n
+echo "$n%"
